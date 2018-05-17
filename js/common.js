97bde89710df72b7353f102b2a21f70e9de01593
Alexandr Kutsenko <accauamoro@gmail.com>
2018-05-17 22:03:24 +0200

    preloader and parallax

--- js/common.js
+++ js/common.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 
+    $(".main_head").css("min-height", $(window).height());
 	
 	
+});
+
+$(window).load(function() {
+
+    $(".loader_inner").delay(600).fadeOut();
+    $(".loader").delay(600).fadeOut("slow");
+
 });
